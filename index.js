87618d722265d7630676664fd0168955993516dc
Einhander <supermario3ebros@gmail.com>
2023-06-13 01:30:46 -0600

    Added a new project to my test server: Project Ellipsy

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 // referenciando el modulo express
 const express= require('express');
-const {readFile} = require("fs");
+const {readFile} = require("fs").promises;
 
 // Inicializando el servidor
 const app = express();
 
 // defualt	
-app.get("/", (resquest, response) => {
-
+app.get("/news", async (resquest, response) => {
 	// Leemos el archivo html
-	readFile("PagWeb/PERIODICO.HTML", "utf-8", (error, data) => {
-
-		if ( error ) {
-			response.status(500).send("An error ocurred...");
-			console.log(error);
-		}
-
-		response.send(data);
-	} );
-
+	try {
+		response.send( await readFile("PagWeb/PERIODICO.HTML", "utf-8") );
+	} catch (error) {
+		response.status(500).send("Sorry, an error ocurried...");
+		console.log(`So, this ocurried... ${error}`);
+	}
 });
 
+// My best project, ellipsy 
+app.get("/", async (resquest, response) => {
+	// Sending my Ellipsy project
+	try {
+		response.send( await readFile("Project-Ellipsy/index.html", "utf-8"));
+	} catch (error) {
+		response.status(500).send("Sorry, an error ocurried...");
+		console.log(`So, this ocurried... ${error}`);
+	}
+});
 
 // Escuchamos en el puerto 3000, o en el puerto especificado por ENV
 app.listen( process.env.PORT || 3000, () => console.log("Servidor HTTP ejecutándose en http://localhost:3000"));
 
 // Defining the use of "static" elements, such as CSS and JS
 app.use( express.static("PagWeb") );
+app.use( express.static("Project-Ellipsy") );
